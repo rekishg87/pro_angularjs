a7fa8c10ff514ee0feb7823e535791803ecfa066
Rekish <rekishg87@gmail.com>
2015-08-25 15:26:06 +0200

    Completed listing 8-6

--- src/main/webapp/chapters/chapter06/controllers/sportsStore.js
+++ src/main/webapp/chapters/chapter06/controllers/sportsStore.js
@@ -4,9 +4,10 @@
 
 angular.module("sportsStore")
     .constant("dataUrl", "http://localhost:5500/products")
+    .constant("orderUrl", "http://localhost:5500/orders")
     .controller("sportsStoreCtrl",
-        ['$scope', '$http', 'dataUrl',
-            function($scope, $http, dataUrl) {
+        ['$scope', '$http', '$location', 'dataUrl', 'orderUrl', 'cart',
+            function($scope, $http, $location, dataUrl, orderUrl, cart) {
 
                 $scope.data = {};
 
@@ -17,4 +18,19 @@ angular.module("sportsStore")
                    .error(function(error) {
                        $scope.data.error = error;
                    });
+
+                $scope.sendOrder = function(shippingDetails) {
+                    var order = angular.copy(shippingDetails);
+                    order.products = cart.getProducts();
+                    $http.post(orderUrl, order)
+                        .success(function(data) {
+                            $scope.data.orderId = data.id;
+                            cart.getProducts().length = 0;
+                        })
+                        .error(function(error) {
+                            $scope.data.orderError = error;
+                        }).finally(function() {
+                            $location.path("/complete");
+                        });
+                }
     }]);
