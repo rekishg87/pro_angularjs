5440e43f5c5d8764554fab4223da478ea3169729
rekishg87 <rekishg87@gmail.com>
2015-08-28 23:02:19 +0200

    Completed listing14-18

---
+++ src/main/webapp/chapters/chapter14/filters/customFilters.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Rekish on 28-8-2015.
+ */
+
+angular.module("exampleApp")
+    .filter("labelCase", function() {
+        return function(value, reverse) {
+            if(angular.isString(value)) {
+                var intermediate = reverse ? value.toUpperCase() : value.toLowerCase();
+                return (reverse ? intermediate[0].toLowerCase() :
+                    intermediate[0].toUpperCase()) + intermediate.substr(1);
+            } else {
+                return value;
+            }
+        };
+    });
