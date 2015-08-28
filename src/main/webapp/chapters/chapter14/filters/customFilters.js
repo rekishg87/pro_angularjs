b5f0b27da228a5f24e0b47214743898ebcf8e3c7
rekishg87 <rekishg87@gmail.com>
2015-08-28 23:12:17 +0200

    Completed listing14-21

--- src/main/webapp/chapters/chapter14/filters/customFilters.js
+++ src/main/webapp/chapters/chapter14/filters/customFilters.js
@@ -13,4 +13,17 @@ angular.module("exampleApp")
                 return value;
             }
         };
+    })
+    .filter("skip", function() {
+        return function(data, count) {
+            if(angular.isArray(data) && angular.isNumber(count)) {
+                if(count > data.length || count < 1) {
+                    return data;
+                } else {
+                    return data.slice(count);
+                }
+            } else {
+                return data;
+            }
+        }
     });
