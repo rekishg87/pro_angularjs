3d8507cddbb4e8e6cd738d7eb8409664ed968975
Rekish <rekishg87@gmail.com>
2015-09-01 14:24:58 +0200

    Completed listing18-10

--- src/main/webapp/chapters/chapter18/services/services.js
+++ src/main/webapp/chapters/chapter18/services/services.js
@@ -2,25 +2,12 @@
  * Created by Rekish on 9/1/2015.
  */
 
-var baseLogger = function() {
-    this.messageCount = 0;
-    this.log = function(msg) {
-        console.log(this.msgType + ": " + (this.messageCount++) + " " + msg);
-    }
-};
-
-var debugLogger = function() {
-
-};
-debugLogger.prototype = new baseLogger();
-debugLogger.prototype.msgType = "Debug";
-
-var errorLogger = function() {
-
-};
-errorLogger.prototype = new baseLogger();
-errorLogger.prototype.msgType = "Error";
-
 angular.module("customServices", [])
-    .service("logService", debugLogger)
-    .service("errorService", errorLogger);
+    .service("logService", function() {
+        return {
+            messageCount: 0,
+            log: function(msg) {
+                console.log("Debug: " + (this.messageCount++) + " " + msg);
+            }
+        };
+    });
