b099250038b4a678259efa1db6f895204ed592ad
Rekish <rekishg87@gmail.com>
2015-09-01 16:07:13 +0200

    Completed listing19-10

--- src/main/webapp/chapters/chapter19/services/services.js
+++ src/main/webapp/chapters/chapter19/services/services.js
@@ -3,37 +3,12 @@
  */
 
 angular.module("customServices", [])
-    .provider("logService", function() {
-        var counter = true;
-        var debug = true;
+    .factory("logService", function ($log) {
+        var messageCount = 0;
         return {
-            messageCounterEnabled: function(setting) {
-                if(angular.isDefined(setting)) {
-                    counter = setting;
-                    return this;
-                } else {
-                    return counter;
-                }
-            },
-            debugEnabled: function(setting) {
-                if(angular.isDefined(setting)) {
-                    debug = setting;
-                    return this;
-                } else {
-                    return debug;
-                }
-            },
-            $get: function($log) {
-                return {
-                    messageCount: 0,
-                    log: function(msg) {
-                        if(debug) {
-                            $log.log("(LOG"
-                                + (counter ? " + " + this.messageCount++ + ") " : ")")
-                                + msg);
-                        }
-                    }
-                };
+            log: function (msg) {
+                $log.log("(LOG + " + this.messageCount++ + ") " + msg);
             }
-        }
+        };
     });
+
