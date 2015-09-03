0809928a163a1ac2f4dd503e1a619ae5bfa492d7
Rekish <rekishg87@gmail.com>
2015-09-03 16:14:58 +0200

    Fixed TypeO listing21-05

--- src/main/webapp/chapters/chapter21/scripts/products.js
+++ src/main/webapp/chapters/chapter21/scripts/products.js
@@ -3,16 +3,15 @@
  */
 
 angular.module("exampleApp", [])
-    .controller("defaultCtrl", function($scope) {
+    .constant("baseUrl", "http://localhost:5500/products/")
+    .controller("defaultCtrl", function($scope, $http, baseUrl) {
         $scope.displayMode = "list";
         $scope.currentProduct = null;
 
         $scope.listProducts = function() {
-            $scope.products = [
-                { id: 0, name: "Dummy1", category: "Test", price: 1.25 },
-                { id: 1, name: "Dummy2", category: "Test", price: 2.45 },
-                { id: 2, name: "Dummy3", category: "Test", price: 4.25 }
-            ];
+            $http.get(baseUrl).success(function(data) {
+                $scope.products = data;
+            });
         }
 
         $scope.deleteProduct = function(product) {
