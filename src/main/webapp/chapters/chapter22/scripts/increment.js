447e492eb7ab773088a0850c1e98f168bb8fdd79
Rekish <rekishg87@gmail.com>
2015-09-04 10:21:22 +0200

    Completed listing22-01

---
+++ src/main/webapp/chapters/chapter22/scripts/increment.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Rekish on 9/4/2015.
+ */
+
+angular.module("increment", [])
+    .directive("increment", function() {
+        return {
+            restrict: "E",
+            scope: {
+                item: "=item",
+                property: "@propertyName",
+                restful: "@restful",
+                method: "@methodName"
+            },
+            link: function(scope, element, attrs) {
+                var button = angular.element("<button>").text("+");
+                button.addClass("btn btn-primary btn-xs");
+                element.append(button);
+                button.on("click", function() {
+                    scope.$apply(function() {
+                        scope.item[scope.property]++;
+                        if(scope.restful) {
+                            scope.item[scope.method]();
+                        }
+                    })
+                })
+            },
+        }
+    });
