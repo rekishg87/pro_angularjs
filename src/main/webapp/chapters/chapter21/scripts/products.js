981203df7c476a21f27ed83796a94c1687cb55d2
Rekish <rekishg87@gmail.com>
2015-09-04 09:20:25 +0200

    Completed listing21-08

--- src/main/webapp/chapters/chapter21/scripts/products.js
+++ src/main/webapp/chapters/chapter21/scripts/products.js
@@ -12,7 +12,7 @@ angular.module("exampleApp", [])
             $http.get(baseUrl).success(function(data) {
                 $scope.products = data;
             });
-        }
+        };
 
         $scope.deleteProduct = function(product) {
             var url = baseUrl + product.id;
@@ -38,20 +38,26 @@ angular.module("exampleApp", [])
         }
 
         $scope.updateProduct = function(product) {
-            for(var i = 0; i < $scope.products.length; i++) {
-                if($scope.products[i].id == product.id) {
-                    $scope.products[i] = product;
-                    break;
+            $http({
+                url: baseUrl + product.id,
+                method: "PUT",
+                data: product
+            }).success(function (modifiedProduct) {
+                for (var i = 0; i < $scope.products.length; i++) {
+                    if ($scope.products[i].id == modifiedProduct.id) {
+                        $scope.products[i] = modifiedProduct;
+                        break;
+                    }
                 }
-            }
-            $scope.displayMode = "list";
+                $scope.displayMode = "list";
+            });
         }
 
         $scope.editOrCreateProduct = function(product) {
             $scope.currentProduct =
                 product ? angular.copy(product) : {};
             $scope.displayMode = "edit";
-        }
+        };
 
         $scope.saveEdit = function(product) {
             if(angular.isDefined(product.id)) {
@@ -59,12 +65,13 @@ angular.module("exampleApp", [])
             } else {
                 $scope.createProduct(product);
             }
-        }
+        };
 
         $scope.cancelEdit = function() {
             $scope.currentProduct = {};
             $scope.displayMode = "list";
-        }
+        };
 
         $scope.listProducts();
     });
+
