ffbfa73deb5e14956aa103a9fd5fd074bb594c09
Rekish <rekishg87@gmail.com>
2015-09-04 10:03:13 +0200

    Completed listing21-15

--- src/main/webapp/chapters/chapter21/scripts/products.js
+++ src/main/webapp/chapters/chapter21/scripts/products.js
@@ -2,13 +2,17 @@
  * Created by Rekish on 9/3/2015.
  */
 
-angular.module("exampleApp", ["increment"])
+angular.module("exampleApp", ["increment", "ngResource"])
     .constant("baseUrl", "http://localhost:5500/products/")
     .controller("defaultCtrl", function($scope, $http, $resource, baseUrl) {
         $scope.displayMode = "list";
         $scope.currentProduct = null;
 
-        $scope.productsResource = $resource(baseUrl + ":id", { id: "@id"});
+        $scope.productsResource = $resource(baseUrl + ":id", { id: "@id"},
+            {
+                create: { method: "POST" },
+                save: { method: "PUT" }
+            });
 
         $scope.listProducts = function() {
             $scope.products = $scope.productsResource.query();
