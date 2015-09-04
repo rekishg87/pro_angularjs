975813c8fae977fd076feb1e7681c626f4f5dd74
Rekish <rekishg87@gmail.com>
2015-09-04 09:25:57 +0200

    Completed listing21-09

---
+++ src/main/webapp/chapters/chapter21/scripts/increment.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Rekish on 9/4/2015.
+ */
+
+angular.module("increment", [])
+    .directive("increment", function() {
+        return {
+            restrict: "E",
+            scope: {
+                value: "=value"
+            },
+            link: function(scope, element, attrs) {
+                var button = angular.element("<button>").text("+");
+                button.addClass("btn btn-primary btn-xs");
+                element.append(button);
+                button.on("click", function() {
+                    scope.$apply(function() {
+                        scope.value++;
+                    })
+                })
+            },
+        }
+    });
