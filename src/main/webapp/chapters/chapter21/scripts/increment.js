d4c67efaea07a840229a10ff2636f30f8129b038
Rekish <rekishg87@gmail.com>
2015-09-04 10:12:08 +0200

    Completed listing21-17

--- src/main/webapp/chapters/chapter21/scripts/increment.js
+++ src/main/webapp/chapters/chapter21/scripts/increment.js
@@ -7,7 +7,10 @@ angular.module("increment", [])
         return {
             restrict: "E",
             scope: {
-                value: "=value"
+                item: "=item",
+                property: "@propertyName",
+                restful: "@restful",
+                method: "@methodName"
             },
             link: function(scope, element, attrs) {
                 var button = angular.element("<button>").text("+");
@@ -15,7 +18,10 @@ angular.module("increment", [])
                 element.append(button);
                 button.on("click", function() {
                     scope.$apply(function() {
-                        scope.value++;
+                        scope.item[scope.property]++;
+                        if(scope.restful) {
+                            scope.item[scope.method]();
+                        }
                     })
                 })
             },
