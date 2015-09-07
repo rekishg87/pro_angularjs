0be7126a743dae39401530530c377e0844077836
Rekish <rekishg87@gmail.com>
2015-09-07 14:15:16 +0200

    Completed listing25-17

---
+++ src/main/webapp/chapters/chapter25/tests/serviceTest.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Rekish on 9/7/2015.
+ */
+
+describe("Service Tests", function() {
+
+    beforeEach(angular.mock.module("exampleApp"));
+
+    it("Increments the counter", function() {
+        angular.mock.inject(function(counterService) {
+            expect(counterService.getCounter()).toEqual(0);
+            counterService.incrementCounter();
+            expect(counterService.getCounter()).toEqual(1);
+        });
+    });
+});
