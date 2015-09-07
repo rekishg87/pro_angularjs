f0ad70763bd5b3148aedd485d4a4c64bbd16f5d8
Rekish <rekishg87@gmail.com>
2015-09-07 11:34:07 +0200

    Completed listing25-05

--- src/main/webapp/chapters/chapter25/scripts/app.js
+++ src/main/webapp/chapters/chapter25/scripts/app.js
@@ -3,7 +3,11 @@
  */
 
 angular.module("exampleApp", [])
-    .controller("defaultCtrl", function($scope) {
+    .controller("defaultCtrl", function($scope, $http) {
+
+        $http.get("productData.json").success(function (data) {
+            $scope.products = data;
+        });
 
         $scope.counter = 0;
 
