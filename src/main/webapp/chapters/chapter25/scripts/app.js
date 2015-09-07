ae4b427e8bf131b5b2122104239ff0d69edd2b5a
Rekish <rekishg87@gmail.com>
2015-09-07 14:11:33 +0200

    Completed listing25-16

--- src/main/webapp/chapters/chapter25/scripts/app.js
+++ src/main/webapp/chapters/chapter25/scripts/app.js
@@ -49,4 +49,15 @@ angular.module("exampleApp", [])
                 }
             }
         }
+    })
+    .factory("counterService", function() {
+        var counter = 0;
+        return {
+            incrementCounter: function() {
+                counter++;
+            },
+            getCounter: function() {
+                return counter;
+            }
+        }
     });
